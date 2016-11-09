31111a374dde296812db44ac0dfde7faa1df0554
Dylan Righi <dylan.righi@noaa.gov>
2016-11-08 16:30:28 -0800

    SetUp.py for vessels

--- Setup_TAP.py
+++ Setup_TAP.py
@@ -103,8 +103,8 @@ RunFiles = []
 
 
 # # Length of release in hours  (0 for instantaneous)
-ReleaseLength = 30*24   # platforms
-# ReleaseLength = 6       # vessels
+# ReleaseLength = 30*24   # platforms
+ReleaseLength = 6       # vessels
 # ReleaseLength = 48      # pipelines
 
 
@@ -148,8 +148,8 @@ TrajectoriesPath = "Trajectories_n" + str(NumLEs) # relative to RootDir
 CubesPath = "Cubes_n" + str(NumLEs)
 CubesRootNames = ["Arc_" for i in StartTimeFiles] # built to match the start time files
 
-CubeStartSitesFilename = os.path.join(RootDir, "Arctic_platforms_all2.txt")
-# CubeStartSitesFilename = os.path.join(RootDir, "Arctic_vessels.txt")
+# CubeStartSitesFilename = os.path.join(RootDir, "Arctic_platforms_all2.txt")
+CubeStartSitesFilename = os.path.join(RootDir, "Arctic_vessels.txt")
 # CubeStartSitesFilename = os.path.join(RootDir, "Arctic_pipelines.txt")
 spos = open(os.path.join(RootDir,CubeStartSitesFilename)).readlines()
 
